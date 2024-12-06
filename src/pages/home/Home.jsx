233ffb50ed9591d523f5165c66746a377e29ae33
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-12-06 17:50:53 +0600

    ADD SOME BLANK FILE

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,19 @@
+import Header from "../shared/header/Header";
+import Navbar from "../shared/navbar/Navbar";
 
 
 const Home = () => {
     return (
         <div>
-            <h1>This is Home.</h1>
+            <Header></Header>
+            <Navbar></Navbar>
+            <h1 className="font-poppins text-2xl">This is Home.</h1>
+            <div>
+                <div>
+                </div>
+                <div></div>
+                <div></div>
+            </div>
         </div>
     );
 };
---
+++ src/pages/shared/navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+
+
+const Navbar = () => {
+    return (
+        <div>
+            <h2 className="text-lg">This is nav.</h2>
+            
+        </div>
+    );
+};
+
+export default Navbar;
