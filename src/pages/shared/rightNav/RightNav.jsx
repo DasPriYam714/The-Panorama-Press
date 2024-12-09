4971cb2fe999468316cba91fe797eca8ab1aa224
DasPriYam714 <priyamdas.aiub@gmail.com>
2024-12-09 21:10:47 +0600

    add left nav part 3

--- src/pages/shared/rightNav/RightNav.jsx
+++ src/pages/shared/rightNav/RightNav.jsx
@@ -1,5 +1,14 @@
-import { FaGoogle, FaGithub, FaFacebookF, FaTwitter, FaInstagram } from "react-icons/fa";
+import {
+  FaGoogle,
+  FaGithub,
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+} from "react-icons/fa";
 
+import QzPic1 from '../../../assets/swimming.png'
+import QzPic2 from '../../../assets/class.png'
+import QzPic3 from '../../../assets/playground.png'
 
 const RightNav = () => {
   return (
@@ -9,31 +18,46 @@ const RightNav = () => {
 
         <button className="btn btn-outline w-full text-blue-600 mb-2">
           <FaGoogle></FaGoogle>
-          Signin  with Google
+          Signin with Google
         </button>
         <button className="btn btn-outline w-full">
           <FaGithub></FaGithub>
-          Signin  with GitHub
+          Signin with GitHub
         </button>
       </div>
 
       <div className="p-4">
-      <h2 className="text-2xl mb-3">Find Us On</h2>
-      <a className="flex p-4 items-center text-lg border rounded-t-lg" href="">
-        <FaFacebookF className="text-blue-700"></FaFacebookF>
-        <span className="pl-2">Facebook</span>
-
-      </a>
-      <a className="flex p-4 items-center text-lg border-x" href="">
-        <FaTwitter className="text-blue-500"></FaTwitter>
-        <span className="pl-2">Twitter</span>
-
-      </a>
-      <a className="flex p-4 items-center text-lg border rounded-b-lg" href="">
-        <FaInstagram className="text-orange-500"></FaInstagram>
-        <span className="pl-2">Instagram</span>
-
-      </a>
+        <h2 className="text-2xl mb-3">Find Us On</h2>
+        <a
+          className="flex p-4 items-center text-lg border rounded-t-lg"
+          href=""
+        >
+          <FaFacebookF className="text-blue-700"></FaFacebookF>
+          <span className="pl-2">Facebook</span>
+        </a>
+        <a className="flex p-4 items-center text-lg border-x" href="">
+          <FaTwitter className="text-blue-500"></FaTwitter>
+          <span className="pl-2">Twitter</span>
+        </a>
+        <a
+          className="flex p-4 items-center text-lg border rounded-b-lg"
+          href=""
+        >
+          <FaInstagram className="text-orange-500"></FaInstagram>
+          <span className="pl-2">Instagram</span>
+        </a>
+      </div>
+      <div className="p-4">
+      <h2 className="text-2xl mb-3">Q-Zone</h2>
+      <div>
+        <img src={QzPic1} alt="" />
+      </div>
+      <div>
+        <img src={QzPic2} alt="" />
+      </div>
+      <div>
+        <img src={QzPic3} alt="" />
+      </div>
 
       </div>
     </div>
